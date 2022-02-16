3692ee70d319ab253551c0bbd9cbbad4235ea7b3
Luan Victor <luanvitorlima2017@gmail.com>
2022-02-16 06:51:27 -0300

    Create implementos.py

---
+++ implementos.py
@@ -0,0 +1,10 @@
+#Prints
+print("Hello world")
+print('João', 'Maria', "e", "Vinicius")
+
+#separadores
+print("Olá", "mundo", sep="-", end=".")
+
+#exemplo de Separação em Cpf
+print("079", "772", "936", sep=".", end="-")
+print("60")
