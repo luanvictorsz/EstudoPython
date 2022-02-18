a4fa76543409cf28a034bf8fd443108729fa6727
Luan Victor <luanvitorlima2017@gmail.com>
2022-02-17 21:46:34 -0300

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,22 @@
+# Strings - str, são caracteres dentro de aspas, no caso de numeros, as aspas não são necessárias
+
+print("isso é uma string")
+print('isso é outra string')
+print("isso serve para aspas 'dentro' de aspas")
+print(12345)  # isso não é uma string
+
+# faça uma quebra de linha com \n, ex:
+
+print(" quebra \n de linha")
+
+# outros tipos de dados
+# int - inteiro, ex: 1, 2, 3...
+# bool - booleano, ex: true, false
+# float - flutuante, ex: 1.5, 20.7...
+
+# verificando os tipos
+
+print('luan', type('Luan'))
+print(1, type(1))
+print(9.10, type(9.10))
+print(10 == 10, type(10 == 10))
