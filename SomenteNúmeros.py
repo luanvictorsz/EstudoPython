370242f3330442016e9106103cff49a4fe201ada
Luan Victor <luanvitorlima2017@gmail.com>
2022-07-27 10:29:49 -0300

    Update SomenteNúmeros.py

--- SomenteNúmeros.py
+++ SomenteNúmeros.py
@@ -1,10 +1,13 @@
+# Nesse exemplo, funciona a soma de apenas numeros trabalhando no codigo.
+
 num1 = input("Digite um número: ")
-num2 = input("Digite outro Número")
+num2 = input("Digite outro Número: ")
 
 try:
     num1 = float(num1)
     num2 = float(num2)
 
     print(num1 + num2)
+#aqui vemos a expressão responsavel por mandar a mensagem de erro, caso uma letra seja adicionada ao calculo
 except:
     print("ERROR")
