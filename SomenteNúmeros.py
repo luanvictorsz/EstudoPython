4eb70424cd2057f5f94950890d3aaa156530ca95
Luan Victor <luanvitorlima2017@gmail.com>
2022-07-27 10:32:37 -0300

    Update SomenteNúmeros.py

--- SomenteNúmeros.py
+++ SomenteNúmeros.py
@@ -3,11 +3,13 @@
 num1 = input("Digite um número: ")
 num2 = input("Digite outro Número: ")
 
+# aqui definimos que nossas variaveis são de valores flutuantes, isso vai ajudar a evitar que letras sejam definidas ao cálculo.
 try:
     num1 = float(num1)
     num2 = float(num2)
 
     print(num1 + num2)
-#aqui vemos a expressão responsavel por mandar a mensagem de erro, caso uma letra seja adicionada ao calculo
+    
+#aqui vemos a expressão responsavel por mandar a mensagem de erro, caso uma letra seja adicionada ao cálculo.
 except:
     print("ERROR")
